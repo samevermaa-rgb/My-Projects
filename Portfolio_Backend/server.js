b1180e87168b8466f9c7414a854dd5b3385f5a49
Sameer <varmasameer1980@gmail.com>
2026-06-21 20:00:07 +0530

    update

--- Portfolio_Backend/server.js
+++ Portfolio_Backend/server.js
@@ -23,7 +23,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin:  "https://sv-admincontrol.vercel.app",
     methods: [
       "GET",
       "POST",
