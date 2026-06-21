798a446a0de97585a6ab9e48eef08c37a87e4806
Sameer <varmasameer1980@gmail.com>
2026-06-21 12:06:01 +0530

    Update Portfolio

--- Portfolio/my-portfolio/app/_Component/Home.jsx
+++ Portfolio/my-portfolio/app/_Component/Home.jsx
@@ -36,7 +36,7 @@ export default function Home() {
             </Link>
 
             <a
-              href="/resume.pdf"
+              href="/sameer-resume.pdf "
               download
               className={styles.secondaryBtn}
             >
